8d3f24580273469e4b94f7c4278aaccaac5e2c79
Federico Viscomi <federico.viscomi@1spatial.com>
2025-07-14 12:35:55 +0100

    first commit

---
+++ app/vite.config.js
@@ -0,0 +1,16 @@
+// app/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
+  define: {
+    'process.env': {},
+  },
+});
