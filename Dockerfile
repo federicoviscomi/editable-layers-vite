b37f13a599c7122933b7cf082cebe1db21a29ce9
Federico Viscomi <federico.viscomi@1spatial.com>
2025-07-14 14:42:27 +0100

    some progress maybe?

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Dockerfile
-FROM node:20-alpine
+FROM node:22.19.0-alpine
 
 WORKDIR /app
 
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { useState } from "react";
+import { DeckGL } from "deck.gl";
+import {
+  EditableGeoJsonLayer,
+  DrawPolygonMode,
+} from "@deck.gl-community/editable-layers";
+import { Map } from "react-map-gl";
+
+//import { log } from '@deck.gl/core';
+//log.level = 4;
+
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [features, setFeatures] = useState({
+    type: "FeatureCollection",
+    features: [],
+  });
+  const [selectedFeatureIndexes] = useState([]);
+
+  const layer = new EditableGeoJsonLayer({
+    data: features,
+    mode: DrawPolygonMode,
+    selectedFeatureIndexes,
+    onEdit: ({ updatedData }) => {
+      setFeatures(updatedData);
+    },
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          is this really really working?!!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={{
+          doubleClickZoom: false,
+        }}
+        layers={[layer]}
+        getCursor={layer.getCursor.bind(layer)}
+      >
+        <Map
+          mapboxAccessToken="pk.eyJ1IjoiYWtpeWFta2EiLCJhIjoiY2p3aG4zY2Y2MDFyNjQ2bjZ1bTNldjQyOCJ9.uM8bC4cSVnYETymmoonsEg"
+          initialViewState={INITIAL_VIEW_STATE}
+          style={{ width: 600, height: 400 }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
+      </DeckGL>
+    </div>
+  );
 }
 
-export default App
+export default App;
